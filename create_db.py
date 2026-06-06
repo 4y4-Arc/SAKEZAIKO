5669cdbbbc4de043078d1a7a0428942b5bbcb1d1
hayato-06 <ishii0206.hayato@gmail.com>
2026-06-06 09:16:17 +0900

    DB用の処理をしました

---
+++ create_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("stock.db")
+
+conn.execute("""
+CREATE TABLE inventory(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    stock INTEGER
+)
+""")
+
+conn.commit()
+conn.close()
