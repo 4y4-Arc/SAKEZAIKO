0ca0eb0a639f51c65cd5f63a92fe7137a51c1338
hayato-06 <ishii0206.hayato@gmail.com>
2026-06-06 09:15:50 +0900

    ひな形を作りました

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from database import get_connection
+
+app = Flask(__name__)
+
+#ログイン画面
+@app.route("/")
+def login():
+    return render_template("Login.html")
+
+#在庫一覧表示
+@app.route("/inventory")
+def inventory_list():
+    return render_template("Inventory_List.html")
+
+#在庫検索
+@app.route("/search")
+def inventory_search():
+    return render_template("Inventory_Search.html")
+
+#在庫登録・更新
+@app.route("/register")
+def inventory_register():
+    return render_template("Inventory_Register.html")
+
+#おすすめ商品表示
+@app.route("/recommend")
+def recommendation():
+    return render_template("Recommend.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
